878941e7a0f824b3add1b417fd2d1c9d9d54b8da
Rohit Raj <92395739+RohiitRaj@users.noreply.github.com>
2022-10-27 00:52:32 +0530

    Create ll.cpp

---
+++ ll.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int;
+const ll mod = 1e9+7;
+#define take_input freopen("input.txt","r",stdin),freopen("output.txt","w",stdout);
+#define fast_io ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
